d964899703d2966cad8cf44534bf47fff21586e2
AsynchronousGillz <vananger93@gmail.com>
2023-05-10 14:03:44 -0600

    remove dark theme button

--- assets/site.js
+++ assets/site.js
@@ -39,30 +39,17 @@ function getTime(){
     document.getElementById("spacetime").innerHTML = time + " " + date;
 }
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); //add this
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); //add this
-    }    
+function switchThemeToDark() {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark'); //add this
 }
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-if (currentTheme) {
+if (window.matchMedia('(prefers-color-scheme: dark)').matches || currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+    switchThemeToDark();
 }
 
 // get time once so as to avoid 1s delay
